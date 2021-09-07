ec852c39e1dad40c616c4e7198cb3bab3de7638a
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-07 11:43:48 -0400

    Write test to verify the attributes of all models

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -5,4 +5,18 @@ RSpec.describe Ingredient do
     it { should have_many(:dish_ingredients) }
     it { should have_many(:dishes).through(:dish_ingredients) }
   end
+
+  describe 'attributes' do
+    it "has a name" do
+      ingredient1 = Ingredient.create!(name: "Cheese", calories: 100)
+
+      expect(ingredient1.name).to eq("Cheese")
+    end
+
+    it "has calories" do
+      ingredient1 = Ingredient.create!(name: "Cheese", calories: 100)
+
+      expect(ingredient1.calories).to eq(100)
+    end
+  end
 end
