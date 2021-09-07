e09095be09936bcf56df46def293eacf29f33bbd
iPowers <isika.powers@gmail.com>
2021-09-07 12:00:51 -0600

    Add more tests

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -1,23 +1,17 @@
-# When I visit a chef's show page
-# I see the name of that chef
-# And I see a link to view a list of all ingredients that this chef uses in their dishes
-# When I click on that link
-# I'm taken to a chef's ingredient index page
-# and I can see a unique list of names of all the ingredients that this chef uses
- require "rails_helper"
+require "rails_helper"
 
- RSpec.describe "Chef Show Page"do
+RSpec.describe "Chef Show Page"do
    before :each do
      @chef = Chef.create!(name: "Chef Remy")
      @dish = @chef.dishes.create!(name: "Pasta", description: "Meatball pasta")
 
      @ingredient1 = @dish.ingredients.create!(name: "Pasta", calories: 110)
      @ingredient2 = @dish.ingredients.create!(name: "Meatball", calories: 90)
-
+     @ingredient3 = @dish.ingredients.create!(name: "Pasta", calories: 110)
    end
 
    describe "Story 3" do
-     it "can display that chef's name" do
+     it "can display that chef's name and link to all unique ingredients" do
        visit "/chefs/#{@chef.id}"
 
        expect(page).to have_content("Chef Remy")
@@ -26,8 +20,9 @@
 
        click_on "All Ingredients"
 
+       expect(current_path).to eq("/chefs/#{@chef.id}/ingredients")
        expect(page).to have_content(@ingredients1)
-       expect(page).to have_content(@ingredients1)
+       expect(page).to have_content(@ingredients2)
      end
    end
  end
