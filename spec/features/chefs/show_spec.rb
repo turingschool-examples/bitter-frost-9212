cb5ef5a014d88226116d0ce709bef4417a69152d
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-07 11:55:16 -0600

    remove save and open page

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe 'a chefs show page' do
 
   it "shows the 3 most popular ingredients" do
     visit "/chefs/#{@remy.id}"
-save_and_open_page
+
     within('#popular_ingred') do
       expect(page).to have_content("Most Popular Ingredients")
       expect(page).to have_content(@cheese.name)
