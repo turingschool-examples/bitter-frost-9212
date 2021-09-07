0a7384877606853a7b7469fedcae2847e79ee22d
Blake Dempsey <bdempsey864@gmail.com>
2021-09-07 10:43:48 -0600

    Still can't figure out why this is failing, shortened test, still no luck

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,25 +1,20 @@
-# Story 1 of 3
-# As a visitor
-# When I visit a dish's show page
-# I see the dish’s name and description
-# And I see a list of ingredients for that dish
-# And I see the chef's name
 require 'spec_helper'
 
 RSpec.describe 'dishes show page' do
   before(:each) do 
-    # @chef_1 = Chef.create!(name: "Murph")
+    @chef_1 = Chef.create!(name: "Murph")
     @dish_1 = Dish.create!(name: "Spaghetti", description: "noodles and sauce")
     @dish_2 = Dish.create!(name: "Smoothie", description: "fruits blended together")
-    # @ingredient_1 = @dish_1.ingredients.create!(name: "noodles", calories: 200)
-    # @ingredient_2 = @dish_1.ingredients.create!(name: "sauce", calories: 80)
-    # @ingredient_3 = @dish_1.ingredients.create!(name: "strawberries", calories: 30)
-    # @ingredient_4 = @dish_1.ingredients.create!(name: "blueberries", calories: 40)
+    @ingredient_1 = @dish_1.ingredients.create!(name: "noodles", calories: 200)
+    @ingredient_2 = @dish_1.ingredients.create!(name: "sauce", calories: 80)
+    @ingredient_3 = @dish_1.ingredients.create!(name: "strawberries", calories: 30)
+    @ingredient_4 = @dish_1.ingredients.create!(name: "blueberries", calories: 40)
   end
 
   it 'shows dish name and description' do
+    require "pry"; binding.pry
     visit "/dishes/#{@dish_1.id}"
-    
+
     expect(page).to have_content(@dish_1.name)
     expect(page).to have_content(@dish_2.name)
     expect(page).to have_content(@dish_1.description)
