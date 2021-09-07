677d7fb7734ec39cc98b4bd74d9e98120c1ffa8c
Erin Quinn <equinn125@gmail.com>
2021-09-07 11:31:54 -0600

    add chef name test and impl to chef ingredients index

--- app/controllers/chefs/ingredients_controller.rb
+++ app/controllers/chefs/ingredients_controller.rb
@@ -1,4 +1,5 @@
 class Chefs::IngredientsController < ApplicationController
   def index
+    @chefs = Chef.all
   end
 end
---
+++ spec/features/chefs/ingredients/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'chef show page' do
+  before :each do
+    @chef = Chef.create!(name: "Chef Remi")
+    @dish_1 = @chef.dishes.create!(name: "Pasta Al Dente", description: "Fresh pasta with your favorite sauce")
+    @ingredient_1 = Ingredient.create!(name: "Mozzarella", calories: 70)
+    @ingredient_2 = Ingredient.create!(name: "Rigatoni", calories: 10)
+    @dish_1.ingredients << @ingredient_1
+    @dish_1.ingredients << @ingredient_2
+  end
+
+  it 'shows a uniqe list of ingredients the chef uses' do
+    visit "/chefs/#{@chef.id}/ingredients"
+    expect(page).to have_content(@chef.name)
+  end
+end
