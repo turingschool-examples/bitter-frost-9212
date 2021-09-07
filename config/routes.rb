73a1e4b4fd5c7465c7290c55536502e73aa08a53
Tanner Dale <tanner@tannerdale.com>
2021-09-07 12:18:37 -0500

    Add some notes in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,11 @@ Rails.application.routes.draw do
   resources :chefs, only: :show do
     resources :ingredients, only: :index
   end
+
+  # I used nested routes here for practice as we never interact
+  # with ingredients outside of the context of a specific chef.
+  # Should we want to expand the application outside of the requirments
+  # for the IC, I would go with:
+  # get '/chefs/:chef_id/ingredients' to: 'chef_ingredients#index'
+  # or create an un-nested ingredients resource, depending on the situation.
 end
