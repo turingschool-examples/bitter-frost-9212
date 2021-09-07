4214a9bafcb0de9baeef072ab7e5f76441ba1195
Khoi Nguyen <khoinguyen311@gmail.com>
2021-09-07 09:29:39 -0700

    Add chef name to spec

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 describe 'chef show page' do
+  before(:each) do
+    @chef = Chef.create!(name: "Chef Khoi")
+  end
   
+  it 'has the name of the chef' do
+    visit "/chefs/#{@chef.id}"
+
+    expect(page).to have_content(@chef.name)
+  end
 end
