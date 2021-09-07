99e1fb8059ccc5743968b4f29722ef64f4a44f3c
Ozzie <mikeosmonson@gmail.com>
2021-09-07 10:07:07 -0600

    Add Dish attributes & chef name to show page

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Dishes Show Page' do
   before :each do
-    @ozz = Chef.create(name: 'oz')
-    @ozz << @dish1 = Dish.create(name: 'pizza', description: 'slice of za')
-    @peperoni << @peperoni = Ingredients.create(name: 'peperoni', calories: 50)
-    @cheese << @cheese = Ingredients.create(name: 'cheese', calories: 100)
+    @ozz = Chef.create!(name: 'oz')
+    @dish1 = @ozz.dishes.create!(chef_id: @ozz.id, name: 'pizza', description: 'slice of za')
+    # @peperoni << @peperoni = Ingredients.create(name: 'peperoni', calories: 50)
+    # @cheese << @cheese = Ingredients.create(name: 'cheese', calories: 100)
   end
 
   it 'show page includes a dish name and description' do
-    visit("/Dishes/#{@dish1}")
-
-    expect(page).to have_content(@dish1.name)
-    expect(page).to have_content(@dish1.description)
+    visit("/dishes/#{@dish1.id}")
+    expect(page).to have_content('pizza')
+    expect(page).to have_content('slice of za')
+    expect(page).to have_content('oz')
     expect(page).to have_content(@peperoni.name)
+    save_and_open_page
     expect(page).to have_content(@cheese.name)
-    expect(page).to have_content(@ozz.name)
   end
 end
