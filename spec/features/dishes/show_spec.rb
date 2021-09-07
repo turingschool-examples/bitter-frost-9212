109f2fa701aa40283b6c45517e07442d378b2d97
Blake Dempsey <bdempsey864@gmail.com>
2021-09-07 10:04:35 -0600

    Getting an error, saying Chef must exist. not sure what to do

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -8,21 +8,21 @@ require 'spec_helper'
 
 RSpec.describe 'dishes show page' do
   before(:each) do 
-    @chef_1 = Chef.create!(name: "Murph")
+    # @chef_1 = Chef.create!(name: "Murph")
     @dish_1 = Dish.create!(name: "Spaghetti", description: "noodles and sauce")
     @dish_2 = Dish.create!(name: "Smoothie", description: "fruits blended together")
-    @ingredient_1 = @dish_1.ingredients.create!(name: "noodles", calories: 200)
-    @ingredient_2 = @dish_1.ingredients.create!(name: "sauce", calories: 80)
-    @ingredient_3 = @dish_1.ingredients.create!(name: "strawberries", calories: 30)
-    @ingredient_4 = @dish_1.ingredients.create!(name: "blueberries", calories: 40)
-
-    visit "/dishes/#{@dish_1.id}"
+    # @ingredient_1 = @dish_1.ingredients.create!(name: "noodles", calories: 200)
+    # @ingredient_2 = @dish_1.ingredients.create!(name: "sauce", calories: 80)
+    # @ingredient_3 = @dish_1.ingredients.create!(name: "strawberries", calories: 30)
+    # @ingredient_4 = @dish_1.ingredients.create!(name: "blueberries", calories: 40)
   end
 
-  it 'lists ingredients for a dish' do
-    expect(page).to have_content(@ingredient_1.name)
-    expect(page).to have_content(@ingredient_2.name)
-    expect(page).to have_content(@chef_1.name)
+  it 'shows dish name and description' do
+    visit "/dishes/#{@dish_1.id}"
+    
     expect(page).to have_content(@dish_1.name)
+    expect(page).to have_content(@dish_2.name)
+    expect(page).to have_content(@dish_1.description)
+    expect(page).to have_content(@dish_2.description)
   end
 end
