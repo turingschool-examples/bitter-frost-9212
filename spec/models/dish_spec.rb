9902b2b1f59689d54e10aaeb77dc1b3d89639910
Christina Delpone <cdelpone@gmail.com>
2021-09-07 10:33:31 -0600

    Test dish model method to count total calories in a dish

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Dish, type: :model do
     it { should have_many(:ingredients).through(:dish_ingredients) }
     it { should have_many(:dish_ingredients) }
   end
+
+  it 'sums calories' do
+    def calorie_count
+      chef = Chef.create!(name: 'Chef Remy')
+      dish = Dish.create!(name: 'Pasta Al Dente', description: 'Fresh pasta with your favorite sauce', chef_id: chef.id)
+      ingredient1 = Ingredient.create!( name: 'Mozzarella', calories: 78)
+      ingredient2 = Ingredient.create!( name: 'Basil', calories: 8)
+
+      dish.ingredients << ingredient1
+      dish.ingredients << ingredient2
+
+      expect(dish.total_calories).to eq(86)
+    end
+  end
 end
