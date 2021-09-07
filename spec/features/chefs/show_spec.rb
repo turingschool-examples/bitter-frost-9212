a5441951e26af09d8791ae1aeb963f26a4f0139a
Weston Ellis <ellisweston112@gmail.com>
2021-09-07 10:59:25 -0600

    Write extension test

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -24,6 +24,36 @@ RSpec.describe 'chef show page' do
     expect(current_path).to eq("/chefs/#{chef.id}/ingredients")
     expect(page).to have_content(ingredient1.name, count: 1)
     expect(page).to have_content(ingredient2.name)
+  end
+
+  it 'extension' do
+    chef = Chef.create!(name: 'Bill')
+    dish1 = chef.dishes.create!(name: 'Cereal', description: 'Milk not included')
+    dish2 = chef.dishes.create!(name: 'Just Cereal', description: 'Bowl and Milk not included')
+    dish3 = chef.dishes.create!(name: 'Bowl of Sadness', description: 'no cereal, just sadness')
+    ingredient3 = Ingredient.create!(name: 'Heaps of Sugar', calories: 300)
+    ingredient1 = Ingredient.create!(name: 'Fruit loops', calories: 30)
+    ingredient2 = Ingredient.create!(name: 'Bowl', calories: 0)
+    ingredient4 = Ingredient.create!(name: 'Banana Slices', calories: 50)
+
+    dish1.ingredients << ingredient3
+    dish1.ingredients << ingredient1
+    dish1.ingredients << ingredient2
+
+    dish2.ingredients << ingredient1
+    dish2.ingredients << ingredient3
+
+    dish3.ingredients << ingredient2
+    dish3.ingredients << ingredient4
+
+    visit "/chefs/#{chef.id}"
+
+    expect(page).to have_content(ingredient1.name)
+    expect(page).to have_content(ingredient2.name)
+    expect(page).to have_content(ingredient3.name)
+    expect(page).to_not have_content(ingredient4.name)
+    expect(ingredient3.name).to appear_before(ingredient2.name)
+    expect(ingredient3.name).to appear_before(ingredient1.name)
 
   end
 
