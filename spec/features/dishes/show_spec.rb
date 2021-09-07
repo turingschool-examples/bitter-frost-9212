1b7b57589f1f2a8b64a95d87c82d0fb4888b29fa
Henry Schmid <henry.schmid1@gmail.com>
2021-09-07 10:19:08 -0600

    Add test to show ingredients for dish

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'dish show page' do
+  before :each do
+    @chef = Chef.create(name: 'Guy Fieri')
+    @dish = @chef.dishes.create(name: 'Pizza', description: 'Yummy')
+    @pep = @dish.ingredients.create(name: 'Pepperoni', calories: 100)
+    @cheese = @dish.ingredients.create(name: 'Cheddar', calories: 80)
+
+    visit "/dishes/#{@dish.id}"
+  end
   it "shows the dish and it's attributes" do
-    chef = Chef.create(name: 'Guy Fieri')
-    dish = chef.dishes.create(name: 'Pizza', description: 'Yummy')
+    expect(page).to have_content(@dish.name)
+    expect(page).to have_content(@dish.description)
+    expect(page).to have_content(@dish.chef.name)
+  end
 
-    visit "/dishes/#{dish.id}"
-
-    expect(page).to have_content(dish.name)
-    expect(page).to have_content(dish.description)
-    expect(page).to have_content(dish.chef.name)
+  it 'shows a list of ingredients for the dish' do
+    expect(page).to have_content(@pep.name)
+    expect(page).to have_content(@cheese.name)
+    save_and_open_page
   end
 end
 # As a visitorname:
