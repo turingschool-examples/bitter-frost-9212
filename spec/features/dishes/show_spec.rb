49334e79b4cac4a5f1e556aa5c6d849abe784bcc
Laura Court <laura.mcourt@gmail.com>
2021-09-07 14:56:08 -0400

    Creates dish show page

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Dish do
-  describe 'relationships' do
-    it { should belong_to(:chef) }
-    it { should have_many(:ingredients).through(:dish_ingredients)}
-  end
+RSpec.describe 'dish show page' do
   before :each do
     @chef = Chef.create!(name: "Me")
     @dish_1 = @chef.dishes.create!(name: "chkn parm", description: "mildly delicious")
@@ -18,9 +14,15 @@ RSpec.describe Dish do
     # @ingred_5 = Ingredient.create!(name: "cucumber", calories: 10)
     # @ingred_6 = Ingredient.create!(name: "tofu", calories: 45)
     DishIngredient.create!(dish: @dish_1, ingredient: @ingred_1)
+
+    visit "/dishes/#{@dish_1.id}"
   end
 
-  it 'has chef names' do
-    expect(@dish_1.chef_name).to eq("Me")
+  it 'shows the dishs name and description' do
+    expect(page).to have_content(@dish_1.name)
+    expect(page).to have_content(@dish_1.description)
+    expect(page).to have_content(@ingred_1.name)
+    expect(page).to have_content(@ingred_1.calories)
+    expect(page).to have_content(@chef.name)
   end
 end
