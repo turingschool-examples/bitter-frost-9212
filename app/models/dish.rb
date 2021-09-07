5349a10a951dab8c87d603f19eea779c30fa3d06
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-09-07 11:59:32 -0600

    it2

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,9 @@
 class Dish < ApplicationRecord
   belongs_to :chef
+  has_many :dishes_ingredients
+  has_many :ingredients, through: :dishes_ingredients
+
+  def total_calories
+    self.ingredients
+  end
 end
