2855ecf148d2f9169e1da645ddfaf2a57cf0f3a9
Kevin Mugele <kevin.mugele@gmail.com>
2021-09-07 11:41:55 -0400

    Create many to many relationship, write tests for relationships

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Chef do
   describe 'relationships' do
     it { should have_many(:dishes) }
   end
+
+  describe 'attributes' do
+    it "has a name" do
+      chef1 = Chef.create!(name: "kevin")
+
+      expect(chef1.name).to eq("kevin")
+    end
+  end
 end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -3,5 +3,30 @@ require 'rails_helper'
 RSpec.describe Dish do
   describe 'relationships' do
     it { should belong_to(:chef) }
+    it { should have_many(:dish_ingredients) }
+    it { should have_many(:ingredients).through(:dish_ingredients) }
+  end
+
+  describe 'attributes' do
+    it "has a name" do
+      chef1 = Chef.create!(name: "kevin")
+      dish1 = chef1.dishes.create!(name: "Lasagna", description: "Delicious")
+
+      expect(dish1.name).to eq("Lasagna")
+    end
+
+    it "has a description" do
+      chef1 = Chef.create!(name: "kevin")
+      dish1 = chef1.dishes.create!(name: "Lasagna", description: "Delicious")
+
+      expect(dish1.description).to eq("Delicious")
+    end
+
+    it "has a chef id" do
+      chef1 = Chef.create!(name: "kevin")
+      dish1 = chef1.dishes.create!(name: "Lasagna", description: "Delicious")
+
+      expect(dish1.chef_id).to eq(chef1.id)
+    end
   end
 end
