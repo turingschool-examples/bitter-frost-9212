3a1c09f1f1cef94773a03601a710cfe90d140d98
Weston Ellis <ellisweston112@gmail.com>
2021-09-07 10:31:57 -0600

    Write model test

--- app/controllers/chef_ingredients_controller.rb
+++ app/controllers/chef_ingredients_controller.rb
@@ -2,7 +2,7 @@ class ChefIngredientsController < ApplicationController
 
   def index
     chef = Chef.find(params[:chef_id])
-    @ingredients = Ingredient.ingredient_by_chef(chef)
+    @ingredients = Ingredient.ingredients_by_chef(chef)
   end
 
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe Ingredient do
     it { should have_many(:dishes).through(:dish_ingredients) }
   end
 
+
+  it 'can fetch a chefs ingredients' do
+    chef = Chef.create!(name: 'Bill')
+    dish = chef.dishes.create!(name: 'Cereal', description: 'Milk not included')
+    ingredient1 = Ingredient.create!(name: 'Fruit loops', calories: 30)
+    ingredient2 = Ingredient.create!(name: 'Bowl', calories: 0)
+    ingredient3 = Ingredient.create!(name: 'Heaps of Sugar', calories: 300)
+
+    dish.ingredients << ingredient1
+    dish.ingredients << ingredient2
+
+    expect(Ingredient.ingredients_by_chef).to eq([ingredient1, ingredient2])
+  end
 end
