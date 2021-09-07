fd731c719e065cb8ca9e87ebe23688f9e9d6851f
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-07 11:16:03 -0600

    add ingredients method to chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,7 @@
 class Chef < ApplicationRecord
   has_many :dishes
+
+  def ingredients
+    dishes.select("ingredients.*").joins(:ingredients).uniq.pluck(:name)
+  end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -4,4 +4,28 @@ RSpec.describe Chef do
   describe 'relationships' do
     it { should have_many(:dishes) }
   end
+
+  describe 'instance methods' do
+
+    before :each do
+      @remy = Chef.create!(name: "Remy")
+
+      @pizza = @remy.dishes.create!(name: "Pizza", description: "Veggie supreme")
+      @mac = @remy.dishes.create!(name: "Macaroni & Cheese", description: "Veggie supreme")
+
+      @sauce = @pizza.ingredients.create!(name: "Pizza Sauce", calories: "50")
+      @dough = @pizza.ingredients.create!(name: "Pizza Dough", calories: "100")
+      @cheese = @pizza.ingredients.create!(name: "Vegan Cheese", calories: "200")
+      @olives = @pizza.ingredients.create!(name: "Olives", calories: "20")
+
+      @pasta = @mac.ingredients.create!(name: "Pasta", calories: 200)
+      @mac.ingredients << @cheese
+    end
+
+    describe '.ingredients' do
+      it "returns a list of unique ingredients a chef uses" do
+        expect(@remy.ingredients).to eq([@sauce.name, @dough.name, @cheese.name, @olives.name, @pasta.name])
+      end
+    end
+  end
 end
