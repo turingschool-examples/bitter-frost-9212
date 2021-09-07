a6923f603cd375a451bce4d7f25cb95102ff81ae
Ida Olson <idaolson@gmail.com>
2021-09-07 11:27:14 -0600

    Complete user story 3

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,11 @@
 class Chef < ApplicationRecord
   has_many :dishes
+
+  def ingredients_index
+    ingredients = dishes.flat_map do |dish|
+      dish.ingredients.map do |ingred|
+        ingred.name
+      end
+    end.uniq!
+  end
 end
