29e240be0c56dd443dc3f9c2973a503ab3d78ecc
Laura Court <laura.mcourt@gmail.com>
2021-09-07 14:13:47 -0400

    Set up migrations and relations

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Dish do
   describe 'relationships' do
     it { should belong_to(:chef) }
+    it { should have_many(:ingredients).through(:dish_ingredients)}
   end
 end
