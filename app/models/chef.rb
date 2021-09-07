f8df055496826666ddd750c9b0a24884c9deb1ae
Ida Olson <idaolson@gmail.com>
2021-09-07 11:48:16 -0600

    Add method popular ingredients to chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -8,4 +8,21 @@ class Chef < ApplicationRecord
       end
     end.uniq!
   end
+
+  def popular_ingredients
+    all_ingredients = dishes.flat_map do |dish|
+      dish.ingredients.map do |ingred|
+        ingred.name
+      end
+    end
+
+    all_ingredients_count = Hash.new(0)
+    all_ingredients.each do |ingred|
+      all_ingredients_count[ingred] += 1
+    end
+
+    (all_ingredients_count.sort_by { |ingred,count| count }[-3..-1]).map do |keys,values|
+      keys
+    end
+  end
 end
