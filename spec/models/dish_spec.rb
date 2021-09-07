5f7a65c51090d55bd198bd8c078cc8f02687a953
Christina Delpone <cdelpone@gmail.com>
2021-09-07 09:38:32 -0600

    Test dish ingredient relationships

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Dish do
+RSpec.describe Dish, type: :model do
   describe 'relationships' do
     it { should belong_to(:chef) }
     it { should have_many(:ingredients).through(:dish_ingredients) }
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Ingredient do
+RSpec.describe Ingredient, type: :model do
   describe 'relationships' do
     it { should have_many(:dishes).through(:dish_ingredients) }
     it { should have_many(:dish_ingredients) }
