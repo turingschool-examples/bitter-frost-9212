192b6ff8648bf61efd687067085c06875b63b7bb
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-07 11:25:47 -0600

    Add test for popular ingredients on chef show page

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -5,14 +5,24 @@ RSpec.describe 'chefs show page' do
     @chef_1 = Chef.create!(name: "Ryan Gatorman")
     @chef_2 = Chef.create!(name: "Risky Riskin")
     @dish_1 = Dish.create!(name: "Chilli of Forgiveness", description: "White bean turkey chilli with fall vegetables", chef: @chef_1)
+    @dish_2 = Dish.create!(name: "Breakfast Bowl", description: "Easy breakfast bowl to start any day.", chef: @chef_1)
+    @dish_3 = Dish.create!(name: "Egg Roll in a Bowl", description: "Tasty Asian inspired lunch bowl", chef: @chef_1)
     @ingredient_1 = Ingredient.create!(name: "Ground Turkey", calories: 90)
     @ingredient_2 = Ingredient.create!(name: "Butternut Squash", calories: 75)
     @ingredient_3 = Ingredient.create!(name: "Kale", calories: 50)
     @ingredient_4 = Ingredient.create!(name: "Egg", calories: 50)
     @ingredient_5 = Ingredient.create!(name: "Chorizo", calories: 120)
+    @ingredient_6 = Ingredient.create!(name: "Rice", calories: 90)
     @dish_1.ingredients << @ingredient_1
     @dish_1.ingredients << @ingredient_2
     @dish_1.ingredients << @ingredient_3
+    @dish_2.ingredients << @ingredient_3
+    @dish_2.ingredients << @ingredient_4
+    @dish_2.ingredients << @ingredient_5
+    @dish_3.ingredients << @ingredient_1
+    @dish_3.ingredients << @ingredient_3
+    @dish_3.ingredients << @ingredient_4
+    @dish_3.ingredients << @ingredient_6
   end
 
   it 'displays' do
@@ -20,6 +30,11 @@ RSpec.describe 'chefs show page' do
 
     expect(page).to have_content(@chef_1.name)
     expect(page).to_not have_content(@chef_2.name)
+    expect(page).to have_content(@ingredient_3.name)
+    expect(page).to have_content(@ingredient_4.name)
+    expect(page).to have_content(@ingredient_1.name)
+    expect(@ingredient_3.name).to appear_before(@ingredient_4.name, only_text: true)
+    expect(@ingredient_4.name).to appear_before(@ingredient_1.name, only_text: true)
     expect(page).to have_button("#{@chef_1.name} Trusted Ingredients")
     expect(page).to_not have_button("#{@chef_2.name} Trusted Ingredients")
   end
