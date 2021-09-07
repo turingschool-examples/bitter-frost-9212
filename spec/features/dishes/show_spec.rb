1cecd9a48145368d10832dc8db1ed5d0d270e183
Erin Quinn <equinn125@gmail.com>
2021-09-07 09:52:42 -0600

    add dish info and ingredient impl/test

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'dish show page' do
   before :each do
     @chef = Chef.create!(name: "Chef Remi")
-    @dish_1 = @chef_1.dishes.create!(name: "Pasta Al Dente", description: "Fresh pasta with your favorite sauce")
+    @dish_1 = @chef.dishes.create!(name: "Pasta Al Dente", description: "Fresh pasta with your favorite sauce")
     @ingredient_1 = Ingredient.create!(name: "Mozzarella", calories: 70)
     @ingredient_2 = Ingredient.create!(name: "Rigatoni", calories: 10)
     @dish_1.ingredients << @ingredient_1
@@ -12,5 +12,13 @@ RSpec.describe 'dish show page' do
 
   it 'shows the dish name and description' do
     visit "/dishes/#{@dish_1.id}"
+    expect(page).to have_content(@dish_1.name)
+    expect(page).to have_content(@dish_1.description)
+  end
+
+  it "shows the ingredients do that dish" do
+    visit "/dishes/#{@dish_1.id}"
+    expect(page).to have_content(@ingredient_1.name)
+    expect(page).to have_content(@ingredient_2.name)
   end
 end
