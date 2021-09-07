137de6c102ea72af5cb120efe33d8f94f9fc722b
Christina Delpone <cdelpone@gmail.com>
2021-09-07 09:46:36 -0600

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :dishes
+  resources :ingredients
+  resources :dish_ingredients
 end
+    #               dishes GET    /dishes(.:format)                       dishes#index
+    #                      POST   /dishes(.:format)                       dishes#create
+    #             new_dish GET    /dishes/new(.:format)                   dishes#new
+    #            edit_dish GET    /dishes/:id/edit(.:format)              dishes#edit
+    #                 dish GET    /dishes/:id(.:format)                   dishes#show
+    #                      PATCH  /dishes/:id(.:format)                   dishes#update
+    #                      PUT    /dishes/:id(.:format)                   dishes#update
+    #                      DELETE /dishes/:id(.:format)                   dishes#destroy
+    #          ingredients GET    /ingredients(.:format)                  ingredients#index
+    #                      POST   /ingredients(.:format)                  ingredients#create
+    #       new_ingredient GET    /ingredients/new(.:format)              ingredients#new
+    #      edit_ingredient GET    /ingredients/:id/edit(.:format)         ingredients#edit
+    #           ingredient GET    /ingredients/:id(.:format)              ingredients#show
+    #                      PATCH  /ingredients/:id(.:format)              ingredients#update
+    #                      PUT    /ingredients/:id(.:format)              ingredients#update
+    #                      DELETE /ingredients/:id(.:format)              ingredients#destroy
+    #     dish_ingredients GET    /dish_ingredients(.:format)             dish_ingredients#index
+    #                      POST   /dish_ingredients(.:format)             dish_ingredients#create
+    #  new_dish_ingredient GET    /dish_ingredients/new(.:format)         dish_ingredients#new
+    # edit_dish_ingredient GET    /dish_ingredients/:id/edit(.:format)    dish_ingredients#edit
+    #      dish_ingredient GET    /dish_ingredients/:id(.:format)         dish_ingredients#show
+    #                      PATCH  /dish_ingredients/:id(.:format)         dish_ingredients#update
+    #                      PUT    /dish_ingredients/:id(.:format)         dish_ingredients#update
+    #                      DELETE /dish_ingredients/:id(.:format)         dish_ingredients#destroy
