1e0a0f4368a3b5b8f7584c59b797f51e16a0863b
Weston Ellis <ellisweston112@gmail.com>
2021-09-07 11:11:56 -0600

    Write model test

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -21,4 +21,28 @@ RSpec.describe Ingredient do
 
     expect(Ingredient.ingredients_by_chef(chef)).to eq([ingredient1, ingredient2])
   end
+
+  it 'can fetch only the three most popular ingredients for a chef' do
+    chef = Chef.create!(name: 'Bill')
+    dish1 = chef.dishes.create!(name: 'Cereal', description: 'Milk not included')
+    dish2 = chef.dishes.create!(name: 'Just Cereal', description: 'Bowl and Milk not included')
+    dish3 = chef.dishes.create!(name: 'Bowl of Sadness', description: 'no cereal, just sadness')
+    ingredient3 = Ingredient.create!(name: 'Heaps of Sugar', calories: 300)
+    ingredient1 = Ingredient.create!(name: 'Fruit loops', calories: 30)
+    ingredient2 = Ingredient.create!(name: 'Bowl', calories: 0)
+    ingredient4 = Ingredient.create!(name: 'Banana Slices', calories: 50)
+
+    dish1.ingredients << ingredient3
+    dish1.ingredients << ingredient1
+    dish1.ingredients << ingredient2
+
+    dish2.ingredients << ingredient1
+    dish2.ingredients << ingredient3
+
+    dish3.ingredients << ingredient2
+    dish3.ingredients << ingredient3
+    dish3.ingredients << ingredient4
+
+    expect(Ingredient.popular_ingredients(chef)).to eq([ingredient3, ingredient2, ingredient1])
+  end
 end
