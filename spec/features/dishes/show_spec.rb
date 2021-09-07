93cdeab813c78e4e55aa7de2690191336cd70cc2
Blake Dempsey <bdempsey864@gmail.com>
2021-09-07 11:59:31 -0600

    Very. Frustrated.

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -1,23 +1,20 @@
 require 'spec_helper'
 
 RSpec.describe 'dishes show page' do
-  before(:each) do 
-    @chef_1 = Chef.create!(name: "Murph")
-    @dish_1 = Dish.create!(name: "Spaghetti", description: "noodles and sauce")
-    @dish_2 = Dish.create!(name: "Smoothie", description: "fruits blended together")
-    @ingredient_1 = @dish_1.ingredients.create!(name: "noodles", calories: 200)
-    @ingredient_2 = @dish_1.ingredients.create!(name: "sauce", calories: 80)
-    @ingredient_3 = @dish_1.ingredients.create!(name: "strawberries", calories: 30)
-    @ingredient_4 = @dish_1.ingredients.create!(name: "blueberries", calories: 40)
-  end
-
   it 'shows dish name and description' do
-    require "pry"; binding.pry
-    visit "/dishes/#{@dish_1.id}"
+    chef_1 = Chef.create!(name: "Murph")
+    dish_1 = Dish.create!(name: "Spaghetti", description: "noodles and sauce")
+    dish_2 = Dish.create!(name: "Smoothie", description: "fruits blended together")
+    ingredient_1 = dish_1.ingredients.create!(name: "noodles", calories: 200)
+    ingredient_2 = dish_1.ingredients.create!(name: "sauce", calories: 80)
+    ingredient_3 = dish_1.ingredients.create!(name: "strawberries", calories: 30)
+    ingredient_4 = dish_1.ingredients.create!(name: "blueberries", calories: 40)
 
-    expect(page).to have_content(@dish_1.name)
-    expect(page).to have_content(@dish_2.name)
-    expect(page).to have_content(@dish_1.description)
-    expect(page).to have_content(@dish_2.description)
+    visit "/dishes/#{dish_1.id}"
+
+    expect(page).to have_content(dish_1.name)
+    expect(page).to have_content(dish_2.name)
+    expect(page).to have_content(dish_1.description)
+    expect(page).to have_content(dish_2.description)
   end
 end
