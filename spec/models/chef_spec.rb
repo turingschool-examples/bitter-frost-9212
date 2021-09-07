9c150378ed8056e8d8c2c91172cb5b7e4791f8a3
Erika Kischuk <erika.kischuk@gmail.com>
2021-09-07 11:03:29 -0600

    Add instance method test for all ing and popular ing

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -1,7 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Chef do
+  before(:each) do
+    @chef_1 = Chef.create!(name: "Ryan Gatorman")
+    @dish_1 = Dish.create!(name: "Chilli of Forgiveness", description: "White bean turkey chilli with fall vegetables", chef: @chef_1)
+    @dish_2 = Dish.create!(name: "Breakfast Bowl", description: "Easy breakfast bowl to start any day.", chef: @chef_1)
+    @dish_3 = Dish.create!(name: "Egg Roll in a Bowl", description: "Tasty Asian inspired lunch bowl", chef: @chef_1)
+    @ingredient_1 = Ingredient.create!(name: "Ground Turkey", calories: 90)
+    @ingredient_2 = Ingredient.create!(name: "Butternut Squash", calories: 75)
+    @ingredient_3 = Ingredient.create!(name: "Kale", calories: 50)
+    @ingredient_4 = Ingredient.create!(name: "Egg", calories: 50)
+    @ingredient_5 = Ingredient.create!(name: "Chorizo", calories: 120)
+    @ingredient_6 = Ingredient.create!(name: "Rice", calories: 90)
+    @dish_1.ingredients << @ingredient_1
+    @dish_1.ingredients << @ingredient_2
+    @dish_1.ingredients << @ingredient_3
+    @dish_2.ingredients << @ingredient_3
+    @dish_2.ingredients << @ingredient_4
+    @dish_2.ingredients << @ingredient_5
+    @dish_3.ingredients << @ingredient_1
+    @dish_3.ingredients << @ingredient_3
+    @dish_3.ingredients << @ingredient_4
+    @dish_3.ingredients << @ingredient_6
+  end
   describe 'relationships' do
     it { should have_many(:dishes) }
+  end
+
+  describe 'instance methods' do
+    it 'has list of ingredients' do
+      expect(@chef_1.all_ing).to eq([@ingredient_1, @ingredient_2, @ingredient_3, @ingredient_4, @ingredient_5, @ingredient_6])
+    end
+    
+    it 'has most popular ingredients' do
+      expect(@chef_1.popular_ing).to eq([@ingredient_3, @ingredient_1, @ingredient_4])
+    end
   end
 end
