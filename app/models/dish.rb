037f0bac754222f852a86c236148d16f311481c0
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-07 10:11:41 -0600

    fix many to many associations

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,7 @@
 class Dish < ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
-  has_many :dishes, through: :dish_ingredients
+  has_many :ingredients, through: :dish_ingredients
 
   validates :name, presence: true
   validates :description, presence: true
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Dish do
   describe 'relationships' do
     it { should belong_to(:chef) }
     it {should have_many :dish_ingredients}
-    it {should have_many(:dishes).through(:dish_ingredients)}
+    it {should have_many(:ingredients).through(:dish_ingredients)}
   end
 
   describe 'validations' do
