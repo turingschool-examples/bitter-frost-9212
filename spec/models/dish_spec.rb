4bf95e8417bed3f700e9ec7afe7c7c4e9ab79f0a
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-09-07 10:34:31 -0600

    Update dish test and model

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Dish do
   describe 'relationships' do
+    it { should have_many(:ingredient_dishes) }
     it { should belong_to(:chef) }
+    it { should have_many(:ingredients) }
   end
 end
