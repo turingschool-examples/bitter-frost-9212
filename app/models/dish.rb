8bbc08a575a02cd81dd7e6deec9e849e54577fdd
Weston Ellis <ellisweston112@gmail.com>
2021-09-07 10:04:06 -0600

    Write model method

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,13 @@ class Dish < ApplicationRecord
   belongs_to :chef
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
+
+  def calories
+    # require "pry"; binding.pry
+    if ingredients.count > 0
+      ingredients.sum(:calories)
+    else
+      0
+    end
+  end
 end
