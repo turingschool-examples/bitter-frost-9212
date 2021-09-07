8e78a1ddffd4761af246b8750f5362433382402c
Weston Ellis <ellisweston112@gmail.com>
2021-09-07 10:01:22 -0600

    Write test for model

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe Dish do
     it { should have_many(:dish_ingredients) }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe 'instance methods' do
+
+    it 'can count calories' do
+      chef = Chef.create!(name: 'Bill')
+      dish = chef.dishes.create!(name: 'Cereal', description: 'Milk not included')
+      ingredient1 = Ingredient.create!(name: 'Fruit loops', calories: 30)
+      ingredient2 = Ingredient.create!(name: 'Bowl', calories: 13)
+
+      expect(dish.calories).to eq(0)
+
+      dish.ingredients << ingredient1
+
+      expect(dish.calories).to eq(30)
+
+      dish.ingredients << ingredient2
+
+      expect(dish.calories).to eq(43)
+
+    end
+  end
 end
