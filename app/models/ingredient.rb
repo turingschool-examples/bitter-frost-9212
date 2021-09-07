faa95475ad702ad2fbf0b732350b56908eb3c14e
Henry Schmid <henry.schmid1@gmail.com>
2021-09-07 11:27:30 -0600

    Add feature and model method for showing chef ingredients page

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,8 @@
 class Ingredient < ApplicationRecord
   has_many :dish_ingredients
   has_many :dishes, through: :dish_ingredients
+
+  def self.ingredients_by_chef(chef_id)
+    joins(:dishes).where('dishes.chef_id': chef_id).distinct
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :dishes
   resources :ingredients
   resources :chefs
+
+  get '/chefs/:id/ingredients', to: 'chef_ingredients#index'
 end
