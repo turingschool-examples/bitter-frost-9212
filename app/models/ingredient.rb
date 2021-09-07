ded4ccc8cc85c56490df687e5f64d7f047b1ca17
Weston Ellis <ellisweston112@gmail.com>
2021-09-07 10:47:25 -0600

    Whoops, meant to say passes WITHOUT ruby

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,14 +3,6 @@ class Ingredient < ApplicationRecord
   has_many :dishes, through: :dish_ingredients
 
   def self.ingredients_by_chef(chef)
-    # require "pry"; binding.pry
-    # arr = []
-    # chef.dishes.each do |dish|
-    #   arr << dish.ingredients
-    # end
-    # arr.flatten.uniq
-
-    # require "pry"; binding.pry
     Ingredient.joins(:dishes).where('chef_id = ?', chef.id).distinct
   end
 end
