3be72d5ff5acc3932dfdc34e6795852579096e77
Henry Schmid <henry.schmid1@gmail.com>
2021-09-07 11:27:10 -0600

    Add feature and model tests for showing chef ingredients page

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe 'dish show page' do
+RSpec.describe 'chef show page' do
   before :each do
     @chef = Chef.create(name: 'Guy Fieri')
     @dish = @chef.dishes.create(name: 'Pizza', description: 'Yummy')
     @pep = @dish.ingredients.create(name: 'Pepperoni', calories: 100)
     @cheese = @dish.ingredients.create(name: 'Cheddar', calories: 80)
+    @pineapple = Ingredient.create(name: 'Pineapple', calories: 20)
 
     visit "/chefs/#{@chef.id}"
   end
 
   it 'shows the chef on show page' do
     expect(page).to have_content(@chef.name)
+  end
+
+  it 'has a link to view all of chefs ingredients' do
+    click_link('All Ingredients')
+
+    expect(current_path).to eq("/chefs/#{@chef.id}/ingredients")
+    expect(page).to have_content(@pep.name)
+    expect(page).to have_content(@cheese.name)
+    expect(page).to_not have_content(@pineapple.name)
   end
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Ingredient do
   it { should have_many(:dishes).through(:dish_ingredients) }
   it { should have_many(:dish_ingredients) }
+
+  it 'finds ingredients by chef' do
+    chef = Chef.create(name: 'Guy Fieri')
+    dish1 = chef.dishes.create(name: 'Pizza', description: 'Yummy')
+    dish2 = chef.dishes.create(name: 'Not Pizza', description: 'Not Yummy')
+    pep = dish1.ingredients.create(name: 'Pepperoni', calories: 100)
+    cheese = dish1.ingredients.create(name: 'Cheddar', calories: 80)
+    DishIngredient.create(dish: dish2, ingredient: pep)
+
+    expect(Ingredient.ingredients_by_chef(chef.id)).to eq([pep, cheese])
+  end
 end
